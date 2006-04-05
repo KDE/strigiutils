34f3c6895751ce84a9513e4be8de1c6ae5e299c3
Jos van den Oever <jos@vandenoever.info>
2006-04-05 14:03:30 +0000

    Extended command-line client to read directories too.
    svn path=/branches/work/kde4/playground/libs/archivereader/; revision=526698

--- src/qclient/archivecat.cpp
+++ src/qclient/archivecat.cpp
@@ -3,17 +3,25 @@ int
 main(int argc, char** argv) {
     ArchiveEngineHandler engine;
     for (int i=1; i<argc; ++i) {
-        QFile file(argv[i]);
-        if (!file.open(QIODevice::ReadOnly)) {
-            qDebug("Could not open '%s'\n", argv[i]);
-            return 1;
-        }
-        const int bufsize=10;
-        char buf[bufsize];
-        qint64 read = file.read(buf, bufsize);
-        while (read > 0) {
-            fwrite(buf, read, 1, stdout);
-            read = file.read(buf, bufsize);
+        QFileInfo info(argv[i]);
+        if (info.isDir()) {
+            QDir d(argv[i]);
+            foreach (QString s, d.entryList()) {
+                printf("%s\n", (const char*)s.toUtf8());
+            }
+        } else {
+            QFile file(argv[i]);
+            if (!file.open(QIODevice::ReadOnly)) {
+                qDebug("Could not open '%s'\n", argv[i]);
+                return 1;
+            }
+            const int bufsize=10;
+            char buf[bufsize];
+            qint64 read = file.read(buf, bufsize);
+            while (read > 0) {
+                fwrite(buf, read, 1, stdout);
+                read = file.read(buf, bufsize);
+            }
         }
     }
 	return 0;
