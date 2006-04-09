19ad99f71c6659aa0cdd45555da3dc1caab49d27
Jos van den Oever <jos@vandenoever.info>
2006-04-09 11:38:50 +0000

    Added a dummy unit test.
    svn path=/branches/work/kde4/playground/libs/archivereader/; revision=527785

---
+++ src/streams/tests/inputstreamtest.cpp
@@ -0,0 +1,9 @@
+#include "inputstreamtest.h"
+
+void
+TestInputStream::testStream() {
+	QVERIFY(true);
+}
+
+QTEST_MAIN(TestInputStream)
+#include "inputstreamtest.moc"
---
+++ src/streams/tests/inputstreamtest.h
@@ -0,0 +1,12 @@
+#ifndef INPUTSTREAM_TEST_H
+#define INPUTSTREAM_TEST_H
+
+#include <QtTest/QtTest>
+
+class TestInputStream : public QObject {
+	Q_OBJECT
+private slots:
+	void testStream();
+};
+
+#endif
