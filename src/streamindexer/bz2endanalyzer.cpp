6dfacc0086f471a0c506080c437edb490d8574a9
Jos van den Oever <jos@vandenoever.info>
2006-05-15 21:44:42 +0000

    nicer naming of bz2 streams
    svn path=/branches/work/kde4/playground/libs/archivereader/; revision=541299

--- src/streamindexer/bz2endanalyzer.cpp
+++ src/streamindexer/bz2endanalyzer.cpp
@@ -4,11 +4,13 @@
 #include "tarinputstream.h"
 #include "tarendanalyzer.h"
 #include "streamindexer.h"
+using namespace std;
 using namespace jstreams;
 
 bool
 BZ2EndAnalyzer::checkHeader(const char* header, int32_t headersize) const {
-    return BZ2InputStream::checkHeader(header, headersize);
+    bool v = BZ2InputStream::checkHeader(header, headersize);
+    return v;
 }
 char
 BZ2EndAnalyzer::analyze(std::string filename, jstreams::InputStream *in,
@@ -30,7 +32,20 @@ BZ2EndAnalyzer::analyze(std::string filename, jstreams::InputStream *in,
         return TarEndAnalyzer::staticAnalyze(filename, &stream, depth, indexer,
             idx);
     } else {
-        std::string file = filename+"/bunzipped";
+        std::string file;
+        int p1 = filename.rfind("/");
+        int len = filename.length();
+        if (p1 != string::npos) {
+            int len = filename.length();
+            if (len > 4 && filename.substr(len-4)==".bz2") {
+                file = filename + filename.substr(p1,len-p1-4);
+            } else {
+                file = filename + filename.substr(p1);
+            }
+        } else {
+            // last resort
+            file = filename+"/bunzipped";
+        }
         return indexer->analyze(file, &stream, depth);
     }
 }
