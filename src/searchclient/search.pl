6a98a7ee0d6925b984f393fb50d61076ca7f8660
Jos van den Oever <jos@vandenoever.info>
2006-05-26 21:03:43 +0000

    perl command line search tool
    svn path=/branches/work/kde4/playground/libs/archivereader/; revision=545121

---
+++ src/searchclient/search.pl
@@ -0,0 +1,27 @@
+#! /usr/bin/perl
+use strict;
+use Socket;
+
+my $query = join(" ", @ARGV);
+
+# define the socket location
+my $rendezvous = "$ENV{HOME}/.kitten/socket";
+# 
+socket(SOCK, PF_UNIX, SOCK_STREAM, 0)       || die "socket: $!";
+select(SOCK);
+$|=1;
+select(STDOUT);
+connect(SOCK, sockaddr_un($rendezvous))     || die "connect: $!";
+
+# send the request
+if (length($query)) {
+    print SOCK "query\n$query\n\n";
+} else {
+    print SOCK "getStatus\n\n";
+}
+# read the result
+my $line;
+while (defined($line = <SOCK>)) {
+    print $line;
+}
+close(SOCK) || die "close: $!";
