8e10a300563f335cceedb7e0da20ab3724c85994
Jos van den Oever <jos@vandenoever.info>
2006-07-03 12:39:38 +0000

    Case fix for windows.
    svn path=/trunk/playground/base/strigi/; revision=557507

--- cmake/FindBZip2.cmake
+++ cmake/FindBZip2.cmake
@@ -1,7 +1,7 @@
 # - Try to find BZip2
 # Once done this will define
 #
-#  BZIP2_FOUND - system has BZip2
+#  BZip2_FOUND - system has BZip2
 #  BZIP2_INCLUDE_DIR - the BZip2 include directory
 #  BZIP2_LIBRARY - Link these to use OpenGL and GLU
 #  BZIP2_DEFINITIONS - Compiler switches required for using BZip2
@@ -21,18 +21,18 @@ FIND_LIBRARY(BZIP2_LIBRARY NAMES bz2
 )
 
 IF(BZIP2_INCLUDE_DIR AND BZIP2_LIBRARY)
-   SET(BZIP2_FOUND TRUE)
+   SET(BZip2_FOUND TRUE)
    INCLUDE(CheckLibraryExists)
    check_library_exists(${BZIP2_LIBRARY} BZ2_bzCompressInit "" BZIP2_NEED_PREFIX)
 ENDIF(BZIP2_INCLUDE_DIR AND BZIP2_LIBRARY)
 
-IF(BZIP2_FOUND)
+IF(BZip2_FOUND)
   IF(NOT BZip2_FIND_QUIETLY)
     MESSAGE(STATUS "Found BZip2: ${BZIP2_LIBRARY}")
   ENDIF(NOT BZip2_FIND_QUIETLY)
-ELSE(BZIP2_FOUND)
+ELSE(BZip2_FOUND)
   IF(BZip2_FIND_REQUIRED)
     MESSAGE(FATAL_ERROR "Could not find BZip2")
   ENDIF(BZip2_FIND_REQUIRED)
-ENDIF(BZIP2_FOUND)
+ENDIF(BZip2_FOUND)
 
