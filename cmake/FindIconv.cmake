010c28d7635b74fcdea179f5bf7cc08b188b4934
Jos van den Oever <jos@vandenoever.info>
2006-07-08 17:19:36 +0000

    Add iconv detection. This is useful for compiling under Windows
    svn path=/trunk/playground/base/strigi/; revision=559940

---
+++ cmake/FindIconv.cmake
@@ -0,0 +1,32 @@
+# - Try to find Iconv 
+# Once done this will define 
+# 
+#  ICONV_FOUND - system has Iconv 
+#  ICONV_INCLUDE_DIR - the Iconv include directory 
+#  ICONV_LIBRARY - Link these to use Iconv 
+# 
+ 
+FIND_PATH(ICONV_INCLUDE_DIR iconv.h 
+  /usr/include 
+  /usr/local/include 
+) 
+ 
+FIND_LIBRARY(ICONV_LIBRARY NAMES iconv c
+  PATHS 
+  /usr/lib 
+  /usr/local/lib 
+) 
+ 
+IF(ICONV_INCLUDE_DIR AND ICONV_LIBRARY) 
+   SET(Iconv_FOUND TRUE) 
+ENDIF(ICONV_INCLUDE_DIR AND ICONV_LIBRARY) 
+ 
+IF(Iconv_FOUND) 
+  IF(NOT Iconv_FIND_QUIETLY) 
+    MESSAGE(STATUS "Found Iconv: ${ICONV_LIBRARY}") 
+  ENDIF(NOT Iconv_FIND_QUIETLY) 
+ELSE(Iconv_FOUND) 
+  IF(Iconv_FIND_REQUIRED) 
+    MESSAGE(FATAL_ERROR "Could not find Iconv") 
+  ENDIF(Iconv_FIND_REQUIRED) 
+ENDIF(Iconv_FOUND) 
