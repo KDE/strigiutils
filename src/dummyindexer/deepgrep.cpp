bc25ff250253d63a7e64f0d7df8ed605c187f5fe
Jos van den Oever <jos@vandenoever.info>
2006-11-07 21:57:21 +0000

    Fix string comparison.
    svn path=/trunk/playground/base/strigi/; revision=603119

--- src/dummyindexer/deepgrep.cpp
+++ src/dummyindexer/deepgrep.cpp
@@ -31,7 +31,7 @@ bool
 containsHelp(int argc, char **argv) {
     for (int i=1; i<argc; ++i) {
          if (strcmp(argv[i], "--help") == 0
-             || strcmp(argv[i], "-h")) return true;
+             || strcmp(argv[i], "-h") == 0) return true;
     }
     return false;
 }
