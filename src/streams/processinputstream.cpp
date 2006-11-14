7b633010b5afad8f2ccf5cb7257816501038e961
Jos van den Oever <jos@vandenoever.info>
2006-11-14 22:02:08 +0000

    Add missing declaration of char** environ on Solaris.
    svn path=/trunk/playground/base/strigi/; revision=604976

--- src/streams/processinputstream.cpp
+++ src/streams/processinputstream.cpp
@@ -26,6 +26,8 @@ using namespace std;
 #if defined(__APPLE__)
 # include <crt_externs.h>
 # define environ (*_NSGetEnviron())
+#else
+extern char **environ;
 #endif
 
 ProcessInputStream::ProcessInputStream(const vector<string>& a,
