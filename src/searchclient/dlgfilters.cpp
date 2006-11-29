250a86dc4041f75488d78653d9566b348e1b70c9
Jos van den Oever <jos@vandenoever.info>
2006-11-29 22:20:38 +0000

    Add small explanation to the filter dialog.
    svn path=/trunk/playground/base/strigi/; revision=609264

--- src/searchclient/dlgfilters.cpp
+++ src/searchclient/dlgfilters.cpp
@@ -4,11 +4,14 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QPushButton>
+#include <QLabel>
  
 DlgFilters::DlgFilters(const QList<QPair<bool,QString> >& filters,
             QWidget *parent)
         : QDialog(parent, Qt::Dialog) {
     setWindowTitle(tr("strigiclient - Edit filters"));
+    QLabel* explanation = new QLabel(tr("Define filters that determine which files will be included and which will be excluded from the index, e.g. '*.html' or '.svn/' (do not index directories called '.svn')."));
+    explanation->setWordWrap(true);
     filterwidget = new FilterWidget();
     filterwidget->setFilters(filters);
     QPushButton* ok = new QPushButton(tr("&Ok"));
@@ -17,6 +20,7 @@ DlgFilters::DlgFilters(const QList<QPair<bool,QString> >& filters,
  
     QVBoxLayout* layout = new QVBoxLayout();
     setLayout(layout);
+    layout->addWidget(explanation);
     layout->addWidget(filterwidget);
     QHBoxLayout* hl = new QHBoxLayout();
     layout->addLayout(hl);
