f771dde2ea669dd8b18e1fc105d3ebb67eea2bcd
Laurent Montel <montel@kde.org>
2007-02-28 14:31:37 +0000

    Sync with kdelibs cmake version of FindBZip2
    svn path=/trunk/kdesupport/strigi/; revision=638004

--- cmake/FindBZip2.cmake
+++ cmake/FindBZip2.cmake
@@ -1,42 +1,43 @@
 # - Try to find BZip2
 # Once done this will define
 #
-#  BZip2_FOUND - system has BZip2
+#  BZIP2_FOUND - system has BZip2
 #  BZIP2_INCLUDE_DIR - the BZip2 include directory
-#  BZIP2_LIBRARY - Link these to use OpenGL and GLU
+#  BZIP2_LIBRARIES - Link these to use BZip2
 #  BZIP2_DEFINITIONS - Compiler switches required for using BZip2
 #  BZIP2_NEED_PREFIX - this is set if the functions are prefixed with BZ2_
+
+# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
 #
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
 
-FIND_PATH(BZIP2_INCLUDE_DIR bzlib.h
-  /usr/include
-  /usr/local/include
-)
+IF (BZIP2_INCLUDE_DIR AND BZIP2_LIBRARIES)
+    SET(BZip2_FIND_QUIETLY TRUE)
+ENDIF (BZIP2_INCLUDE_DIR AND BZIP2_LIBRARIES)
 
-FIND_LIBRARY(BZIP2_LIBRARY NAMES bz2 bzip2
-  PATHS
-  /usr/lib/
-  /usr/lib/local/lib/
-)
+FIND_PATH(BZIP2_INCLUDE_DIR bzlib.h )
 
-IF(BZIP2_INCLUDE_DIR AND BZIP2_LIBRARY)
-   SET(BZip2_FOUND TRUE)
+FIND_LIBRARY(BZIP2_LIBRARIES NAMES bz2 bzip2 )
+
+IF (BZIP2_INCLUDE_DIR AND BZIP2_LIBRARIES)
+   SET(BZIP2_FOUND TRUE)
    INCLUDE(CheckLibraryExists)
-   check_library_exists(${BZIP2_LIBRARY} BZ2_bzCompressInit "" BZIP2_NEED_PREFIX)
-ENDIF(BZIP2_INCLUDE_DIR AND BZIP2_LIBRARY)
+   CHECK_LIBRARY_EXISTS(${BZIP2_LIBRARIES} BZ2_bzCompressInit "" BZIP2_NEED_PREFIX)
+ELSE (BZIP2_INCLUDE_DIR AND BZIP2_LIBRARIES)
+   SET(BZIP2_FOUND FALSE)
+ENDIF (BZIP2_INCLUDE_DIR AND BZIP2_LIBRARIES)
 
-IF(BZip2_FOUND)
-  IF(NOT BZip2_FIND_QUIETLY)
-    MESSAGE(STATUS "Found BZip2: ${BZIP2_LIBRARY}")
-  ENDIF(NOT BZip2_FIND_QUIETLY)
-ELSE(BZip2_FOUND)
-  IF(BZip2_FIND_REQUIRED)
-    MESSAGE(FATAL_ERROR "Could not find BZip2")
-  ENDIF(BZip2_FIND_REQUIRED)
-ENDIF(BZip2_FOUND)
+IF (BZIP2_FOUND)
+  IF (NOT BZip2_FIND_QUIETLY)
+    MESSAGE(STATUS "Found BZip2: ${BZIP2_LIBRARIES}")
+  ENDIF (NOT BZip2_FIND_QUIETLY)
+ELSE (BZIP2_FOUND)
+  IF (BZip2_FIND_REQUIRED)
+    MESSAGE(FATAL_ERROR "Could NOT find BZip2")
+  ENDIF (BZip2_FIND_REQUIRED)
+ENDIF (BZIP2_FOUND)
 
-MARK_AS_ADVANCED(
-  BZIP2_INCLUDE_DIR
-  BZIP2_LIBRARY
-)
+MARK_AS_ADVANCED(BZIP2_INCLUDE_DIR BZIP2_LIBRARIES)
+
