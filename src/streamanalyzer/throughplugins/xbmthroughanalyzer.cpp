d1c7add1d487f4d9f43f1b71bedacb7b969f450d
Vincent Ricard <magic@magicninja.org>
2007-03-19 20:11:21 +0000

    port the xbm plugin (add 'xhot' and 'yhot' info, which were not in the kfile version)
    svn path=/trunk/kdesupport/strigi/; revision=644360

---
+++ src/streamanalyzer/throughplugins/xbmthroughanalyzer.cpp
@@ -0,0 +1,162 @@
+/* This file is part of Strigi Desktop Search
+ *
+ * Copyright (C) 2007 Vincent Ricard <magic@magicninja.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#include "jstreamsconfig.h"
+#include "xbmthroughanalyzer.h"
+#include "analysisresult.h"
+#include "fieldtypes.h"
+
+using namespace std;
+using namespace jstreams;
+using namespace Strigi;
+
+// AnalyzerFactory
+const cnstr XbmThroughAnalyzerFactory::widthFieldName("width");
+const cnstr XbmThroughAnalyzerFactory::heightFieldName("height");
+const cnstr XbmThroughAnalyzerFactory::xHotFieldName("xhot");
+const cnstr XbmThroughAnalyzerFactory::yHotFieldName("yhot");
+
+void
+XbmThroughAnalyzerFactory::registerFields(FieldRegister& reg) {
+    widthField = reg.registerField(widthFieldName,
+        FieldRegister::integerType, 1, 0);
+    heightField = reg.registerField(heightFieldName,
+        FieldRegister::integerType, 1, 0);
+    xHotField = reg.registerField(xHotFieldName,
+        FieldRegister::integerType, 1, 0);
+    yHotField = reg.registerField(yHotFieldName,
+        FieldRegister::integerType, 1, 0);
+}
+
+// Analyzer
+void
+XbmThroughAnalyzer::setIndexable(AnalysisResult* i) {
+    analysisResult = i;
+}
+
+/**
+ * @returns the next offset, @c -1 if an error occured.
+ */
+int32_t
+processLine(const char* buf, int32_t bufSize, int32_t offset, int32_t& data) {
+    // check we have a '#define'
+    if (strncmp(buf + offset, "#define", 7)) {
+        return -1;
+    }
+
+    // skip spaces
+    int32_t i = offset + 7;
+    while (i < bufSize && (buf[i] == ' ' || buf[i] == '\t')) {
+        i++;
+    }
+    if (i >= bufSize)
+        return -1;
+
+    // skip the name of the define (should be /.*_width/ or /.*_height/ or /.*_x_hot/ or /.*_y_hot/)
+    while (i < bufSize && !(buf[i] == ' ' || buf[i] == '\t')) {
+        i++;
+    }
+    if (i >= bufSize)
+        return -1;
+
+    // skip spaces
+    while (i < bufSize && (buf[i] == ' ' || buf[i] == '\t')) {
+        i++;
+    }
+    // check if we have a valid data
+    if (i >= bufSize || !isdigit(buf[i]))
+        return -1;
+
+    // read the data
+    data = 0;
+    while (i < bufSize && isdigit(buf[i])) {
+        data = (data * 10) + buf[i] - '0';
+        i++;
+    }
+    if (i >= bufSize)
+        return -1;
+
+    // skip eol
+    while (i < bufSize && (buf[i] == '\n' || buf[i] == '\r')) {
+        i++;
+    }
+
+    return i;
+}
+
+InputStream*
+XbmThroughAnalyzer::connectInputStream(InputStream* in) {
+    const int32_t nreq = 16;
+    int32_t nextOffset = 0;
+    const char* buf;
+
+    int32_t nread = in->read(buf, nreq, -1);
+    in->reset(0);
+
+    if (nread < nreq) {
+        return in;
+    }
+
+    int32_t width;
+    nextOffset = processLine(buf, nread, nextOffset, width);
+    if (nextOffset == -1) {
+        return in;
+    }
+    int32_t height;
+    nextOffset = processLine(buf, nread, nextOffset, height);
+    if (nextOffset == -1) {
+        return in;
+    }
+
+    analysisResult->setField(factory->widthField, width);
+    analysisResult->setField(factory->heightField, height);
+
+    // these both fields are optional
+    int32_t xHot;
+    nextOffset = processLine(buf, nread, nextOffset, xHot);
+    if (nextOffset != -1) {
+        int32_t yHot;
+        nextOffset = processLine(buf, nread, nextOffset, yHot);
+        if (nextOffset != -1) {
+            analysisResult->setField(factory->xHotField, xHot);
+            analysisResult->setField(factory->yHotField, yHot);
+        }
+    }
+
+    return in;
+}
+
+bool
+XbmThroughAnalyzer::isReadyWithStream() {
+    return true;
+}
+
+//Factory
+class Factory : public AnalyzerFactoryFactory {
+public:
+    list<StreamThroughAnalyzerFactory*>
+    getStreamThroughAnalyzerFactories() const {
+        list<StreamThroughAnalyzerFactory*> af;
+        af.push_back(new XbmThroughAnalyzerFactory());
+        return af;
+    }
+};
+
+STRIGI_ANALYZER_FACTORY(Factory)
---
+++ src/streamanalyzer/throughplugins/xbmthroughanalyzer.h
@@ -0,0 +1,66 @@
+/* This file is part of Strigi Desktop Search
+ *
+ * Copyright (C) 2007 Vincent Ricard <magic@magicninja.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+#ifndef XBMTHROUGHANALYZER
+#define XBMTHROUGHANALYZER
+
+#include "streamthroughanalyzer.h"
+#include "analyzerplugin.h"
+#include "cnstr.h"
+
+namespace Strigi {
+    class RegisteredField;
+}
+class XbmThroughAnalyzerFactory;
+
+class STRIGI_PLUGIN_API XbmThroughAnalyzer
+    : public Strigi::StreamThroughAnalyzer {
+private:
+    Strigi::AnalysisResult* analysisResult;
+    const XbmThroughAnalyzerFactory* factory;
+public:
+    XbmThroughAnalyzer(const XbmThroughAnalyzerFactory* f) :factory(f) {}
+    ~XbmThroughAnalyzer() {}
+    void setIndexable(Strigi::AnalysisResult* i);
+    jstreams::InputStream *connectInputStream(jstreams::InputStream *in);
+    bool isReadyWithStream();
+};
+
+class XbmThroughAnalyzerFactory
+    : public Strigi::StreamThroughAnalyzerFactory {
+friend class XbmThroughAnalyzer;
+private:
+    static const cnstr widthFieldName;
+    static const cnstr heightFieldName;
+    static const cnstr xHotFieldName;
+    static const cnstr yHotFieldName;
+    const Strigi::RegisteredField* widthField;
+    const Strigi::RegisteredField* heightField;
+    const Strigi::RegisteredField* xHotField;
+    const Strigi::RegisteredField* yHotField;
+    const char* getName() const {
+        return "XbmThroughAnalyzer";
+    }
+    Strigi::StreamThroughAnalyzer* newInstance() const {
+        return new XbmThroughAnalyzer(this);
+    }
+    void registerFields(Strigi::FieldRegister&);
+};
+
+#endif
