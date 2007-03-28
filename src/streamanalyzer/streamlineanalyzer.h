8a8ad7509e7f2319ea0058da0f80e92c903148ff
Jos van den Oever <jos@vandenoever.info>
2007-03-28 22:15:43 +0000

    Documentation by Tobias Pfeiffer.
    svn path=/trunk/kdesupport/strigi/; revision=647623

--- src/streamanalyzer/streamlineanalyzer.h
+++ src/streamanalyzer/streamlineanalyzer.h
@@ -1,6 +1,7 @@
 /* This file is part of Strigi Desktop Search
  *
  * Copyright (C) 2007 Jos van den Oever <jos@vandenoever.info>
+ *               2007 Tobias G. Pfeiffer <tgpfeiffer@web.de>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -28,24 +29,79 @@ class RegisteredField;
 class AnalysisResult;
 class FieldRegister;
 
+/**
+ * This class is especially well suited for file formats that are based on
+ * lines of plain text, i.e. where a line break indicates information separation,
+ * such as CSV as opposed to, for example, SVG files, where line breaks do not
+ * necessarily indicate start/end of information blocks.
+ * (However, this class can still be used for that purpose, it just isn't the
+ * best choice.)
+ */
 class STREAMANALYZER_EXPORT StreamLineAnalyzer {
 public:
+    /**
+     * Destructor. Clean up your room :-)
+     */
     virtual ~StreamLineAnalyzer() {}
+    /**
+     * Returns the name of this analyzer. Taking the class name is fine
+     * for this purpose.
+     */
     virtual const char* getName() const = 0;
-    virtual void startAnalysis(AnalysisResult*) = 0;
+    /**
+     * Is called to signal the beginning of a stream analysis. This is the place
+     * to initialize variables that need to be set again for every stream.
+     * If there are single objects that will be needed again for every analyzed
+     * stream, they should be created in the constructor.
+     * \param result pointer to the AnalysisResult to write your results to
+     */
+    virtual void startAnalysis(AnalysisResult* result) = 0;
+    /**
+     * Is called when the analysis of a stream is finished. You can do
+     * cleanups here, if necessary.
+     */
     virtual void endAnalysis() {}
+    /**
+     * Is called for every line in the stream. Here, the actual analysis
+     * takes place.
+     * \param data character data of the line
+     * \param length number of characters in that line
+     */
     virtual void handleLine(const char* data, uint32_t length) = 0;
+    /**
+     * Tells the caller whether you are finished with your analysis or not.
+     * If this returns true, this Analyzer will receive no more data from
+     * the stream, i.e. handleLine() will not be called again.
+     * If this analyzer notices it is not able to use the given data (for
+     * example, this is a completely different file format), have this
+     * function return true ASAP.
+     * \return true if you are finished with this stream, false otherwise
+     */
     virtual bool isReadyWithStream() = 0;
 };
 
+/**
+ * This is the factory for the creation of a StreamLineAnalyzer.
+ */
 class STREAMANALYZER_EXPORT StreamLineAnalyzerFactory {
 public:
     virtual ~StreamLineAnalyzerFactory() {}
+    /**
+     * Returns the name of this analyzer factory. Taking the class name is fine
+     * for this purpose.
+     */
     virtual const char* getName() const = 0;
-    virtual void registerFields(Strigi::FieldRegister&) = 0;
+    /**
+     * Here you should register all fields that the corresponding
+     * StreamLineAnalyzer is able to extract using FieldRegister::registerField().
+     */
+    virtual void registerFields(Strigi::FieldRegister& reg) = 0;
+    /**
+     * Is called to create a new instance of the corresponding StreamLineAnalyzer.
+     * \return pointer to the new analyzer instance
+     */
     virtual StreamLineAnalyzer* newInstance() const = 0;
 };
-
 
 }
 
