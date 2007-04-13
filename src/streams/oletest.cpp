11dfd2ebf56ec4682a9e17b30cb50cd6b64221a6
Jos van den Oever <jos@vandenoever.info>
2007-04-13 19:12:56 +0000

    fix for whining compilers
    svn path=/trunk/kdesupport/strigi/; revision=653602

--- src/streams/oletest.cpp
+++ src/streams/oletest.cpp
@@ -8,17 +8,17 @@ main(int argc, char** argv) {
         fprintf(stderr, "%s\n", argv[i]);
         FileInputStream f(argv[i]);
         OleInputStream o(&f);
-        InputStream* i = o.nextEntry();
-        while (i) {
+        InputStream* j = o.nextEntry();
+        while (j) {
             const char* data;
             int64_t s = 0;
-            int32_t n = i->read(data, 1, 1);
+            int32_t n = j->read(data, 1, 1);
             while (n > 0) {
                 s += n;
-                n = i->read(data, 11111111, 11111);
+                n = j->read(data, 11111111, 11111);
             }
-            fprintf(stderr, "out %lli %lli %i\n", s, i->size(), n);
-            i = o.nextEntry();
+            fprintf(stderr, "out %lli %lli %i\n", s, j->size(), n);
+            j = o.nextEntry();
         }
     }
     return 0;
