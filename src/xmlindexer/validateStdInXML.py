7f402eb6eb7c7724457ff5cadcc17437866e09b5
Jos van den Oever <jos@vandenoever.info>
2007-04-24 18:30:03 +0000

    better error reporting
    svn path=/trunk/kdesupport/strigi/; revision=657671

--- src/xmlindexer/validateStdInXML.py
+++ src/xmlindexer/validateStdInXML.py
@@ -1,9 +1,21 @@
 #! /usr/bin/python
 import sys
-from xml.sax import make_parser 
+from xml.sax import make_parser, handler, SAXException 
+
+class UriLogger(handler.ContentHandler):
+    def startElement(self, name, attrs):
+        if attrs.has_key('uri'):
+            self.uri = attrs['uri']
 
 # this script reads from standard input and parses it as xml
 # if the xml is invalid, it will print an error message
 
 parser = make_parser()
-parser.parse(sys.stdin)
+urilogger = UriLogger()
+parser.setContentHandler(urilogger)
+
+try:
+   parser.parse(sys.stdin)
+except SAXException, e :
+   print "Error after "+urilogger.uri
+   print e;
