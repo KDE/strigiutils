24d220d2b0f755f2fbd964d1ae3800a6cf0967c9
Dirk Mueller <mueller@kde.org>
2007-04-27 13:09:34 +0000

    trying to make the pkgconfig files a bit more working
    svn path=/trunk/kdesupport/strigi/; revision=658495

--- src/streamanalyzer/libstreamanalyzer.pc.cmake
+++ src/streamanalyzer/libstreamanalyzer.pc.cmake
@@ -5,7 +5,7 @@ includedir=${CMAKE_INSTALL_PREFIX}/include
 
 Name: libstreamanalyzer
 Description: C++ library for extracting text and metadata from files and streams
-Requires: libstreams libpthread
+Requires: libstreams
 Version: ${STRIGI_VERSION_MAJOR}.${STRIGI_VERSION_MINOR}.${STRIGI_VERSION_PATCH}
-Libs: -L${libdir} -lstreamanalyzer
-Cflags: -I${includedir} -I${libdir}/include
+Libs: -L${LIB_DESTINATION} -lstreamanalyzer
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
--- src/streams/libstreams.pc.cmake
+++ src/streams/libstreams.pc.cmake
@@ -5,7 +5,6 @@ includedir=${CMAKE_INSTALL_PREFIX}/include
 
 Name: libstreams
 Description: C++ streams for reading data as streams from various file formats
-Requires: libz libbz2 libxml2
 Version: ${STRIGI_VERSION_MAJOR}.${STRIGI_VERSION_MINOR}.${STRIGI_VERSION_PATCH}
-Libs: -L${libdir} -lstreams
-Cflags: -I${includedir} -I${libdir}/include
+Libs: -L${LIB_DESTINATION} -lstreams
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
