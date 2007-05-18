4c7ed719e1bf537c103b3f35e527f9b12b61d5a2
Jos van den Oever <jos@vandenoever.info>
2007-05-18 22:30:19 +0000

    more function calls in the test script
    svn path=/trunk/kdesupport/strigi/; revision=666150

--- src/daemon/dbus/xesamtest.py
+++ src/daemon/dbus/xesamtest.py
@@ -2,26 +2,47 @@
 import dbus
 
 # simple tests on the xesam interface
+xesamSessionFields = ["search.live", "search.blocking", "hit.fields",
+    "hit.snippet.length", "sort.primary", "sort.secondary", "sort.order",
+    "vendor.id", "vendor.version", "vendor.display", "vendor.xesam",
+    "vendor.fieldnames", "vendor.extensions", "nonsense.nonsense"]
 
-# connect
+# function to make a dbus object into a nice string
+def dbusToStr(p):
+    if not isinstance(p, dbus.String):
+        try:
+            p = str([str(string) for string in p])
+        except:
+            p = str(p)
+    return p
+
+# connect and print the status of the indexer
 bus = dbus.SessionBus()
 obj = bus.get_object('org.freedesktop.xesam.searcher',
 	'/org/freedesktop/xesam/searcher/main')
 xesam = dbus.Interface(obj, 'org.freedesktop.xesam.search')
 print xesam.GetState()
+
+# create a session and print the properties of the new session
 session = xesam.NewSession()
-print session
-print xesam.SetProperty(session, "search.live", True)
-print xesam.SetProperty(session, "search.blocking", True)
-print xesam.SetProperty(session, "hit.fields", ["uri"])
-print xesam.SetProperty(session, "hit.snippet.length", 30)
-print xesam.SetProperty(session, "sort.primary", "uri")
-print xesam.SetProperty(session, "sort.secondary", "uri")
-print xesam.SetProperty(session, "sort.order", "descending")
-print xesam.SetProperty(session, "vendor.id", "whatever")
-print xesam.SetProperty(session, "vendor.version", 3)
-print xesam.SetProperty(session, "vendor.display", "0wn3d")
-print xesam.SetProperty(session, "vendor.xesam", "3")
-print xesam.SetProperty(session, "vendor.fieldnames", ["uri", "hi"])
-print xesam.SetProperty(session, "vendor.extensions", ["uri"])
-print xesam.GetHits(session, 3)
+for field in xesamSessionFields:
+    p = xesam.GetProperty(session, field)
+    print field + ":\t" + dbusToStr(p)
+
+search = xesam.NewSearch(session, "<somexml/>")
+
+print session+"\t"+search
+print dbusToStr(xesam.SetProperty(session, "search.live", True))
+print dbusToStr(xesam.SetProperty(session, "search.blocking", True))
+print dbusToStr(xesam.SetProperty(session, "hit.fields", ["uri"]))
+print dbusToStr(xesam.SetProperty(session, "hit.snippet.length", 30))
+print dbusToStr(xesam.SetProperty(session, "sort.primary", "uri"))
+print dbusToStr(xesam.SetProperty(session, "sort.secondary", "uri"))
+print dbusToStr(xesam.SetProperty(session, "sort.order", "descending"))
+print dbusToStr(xesam.SetProperty(session, "vendor.id", "whatever"))
+print dbusToStr(xesam.SetProperty(session, "vendor.version", 3))
+print dbusToStr(xesam.SetProperty(session, "vendor.display", "0wn3d"))
+print dbusToStr(xesam.SetProperty(session, "vendor.xesam", "3"))
+print dbusToStr(xesam.SetProperty(session, "vendor.fieldnames", ["uri", "hi"]))
+print dbusToStr(xesam.SetProperty(session, "vendor.extensions", ["uri"]))
+print xesam.GetHits(search, 3)
