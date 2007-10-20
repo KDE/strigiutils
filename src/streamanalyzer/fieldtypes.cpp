ac725a02c5bf05825539a0275a0090a0d2943f4c
Jos van den Oever <jos@vandenoever.info>
2007-10-20 20:30:45 +0000

    Updated the warning messages to mention rdfs instead of fieldproperties.
    svn path=/trunk/kdesupport/strigi/; revision=727513

--- src/streamanalyzer/fieldtypes.cpp
+++ src/streamanalyzer/fieldtypes.cpp
@@ -103,7 +103,7 @@ FieldRegister::registerField(const string& fieldname) {
             = FieldPropertiesDb::db().properties(fieldname);
         if (!props.valid()) {
             cerr << "WARNING: field '" << fieldname << "' is not defined in "
-                ".fieldproperties ontology database." << endl;
+                "any rdfs ontology database." << endl;
 	    // creates a field with defaults (stringType and no parents)
             FieldPropertiesDb::db().addField(fieldname);
         }
