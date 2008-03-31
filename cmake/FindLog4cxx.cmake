22a8895b0bfa5006a46d479443bf6a62c75dde8e
Flavio Castelli <flavio@castelli.name>
2008-03-31 20:38:17 +0000

    Fixed log4cxx discover & enable
    svn path=/trunk/kdesupport/strigi/; revision=792329

--- cmake/FindLog4cxx.cmake
+++ cmake/FindLog4cxx.cmake
@@ -1,7 +1,7 @@
 # - Try to find the liblog4cxx libraries
 # Once done this will define
 #
-# Log4cxx_FOUND - system has liblog4cxx
+# LOG4CXX_FOUND - system has liblog4cxx
 # LOG4CXX_INCLUDE_DIR - the liblog4cxx include directory
 # LOG4CXX_LIBRARIES - liblog4cxx library
 
@@ -9,7 +9,7 @@ FIND_PATH(LOG4CXX_INCLUDE_DIR logger.h PATHS /usr/include/log4cxx /usr/local/inc
 FIND_LIBRARY(LOG4CXX_LIBRARIES NAMES log4cxx )
 
 IF(LOG4CXX_INCLUDE_DIR AND LOG4CXX_LIBRARIES)
-  SET(Log4cxx_FOUND 1)
+  SET(LOG4CXX_FOUND 1)
   #remove last /log4cxx string
   STRING(REGEX REPLACE "/log4cxx" "" LOG4CXX_INCLUDE_DIR_SUP_LEVEL ${LOG4CXX_INCLUDE_DIR})
   SET (LOG4CXX_INCLUDE_DIR ${LOG4CXX_INCLUDE_DIR_SUP_LEVEL} ${LOG4CXX_INCLUDE_DIR} )
@@ -17,7 +17,7 @@ IF(LOG4CXX_INCLUDE_DIR AND LOG4CXX_LIBRARIES)
    message(STATUS "Found log4cxx: ${LOG4CXX_LIBRARIES}")
   endif(NOT Log4cxx_FIND_QUIETLY)
 ELSE(LOG4CXX_INCLUDE_DIR AND LOG4CXX_LIBRARIES)
-  SET(Log4cxx_FOUND 0 CACHE BOOL "Not found log4cxx library")
+  SET(LOG4CXX_FOUND 0 CACHE BOOL "Not found log4cxx library")
   message(STATUS "NOT Found log4cxx, disabling it")
 ENDIF(LOG4CXX_INCLUDE_DIR AND LOG4CXX_LIBRARIES)
 
