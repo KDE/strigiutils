4d9673d11aff52e72cd7a0ff86169af134137194
Marcus Hufgard <marcus.hufgard@hufgard.de>
2008-12-24 09:04:55 +0000

    find Clucene also in the /usr/lib64 directory
    svn path=/trunk/kdesupport/strigi/; revision=900984

--- cmake/FindCLucene.cmake
+++ cmake/FindCLucene.cmake
@@ -26,6 +26,7 @@ SET(TRIAL_LIBRARY_PATHS
   /usr/local/lib${LIB_SUFFIX}
   /opt/local/lib${LIB_SUFFIX}
   /usr/lib${LIB_SUFFIX}
+  /usr/lib64${LIB_SUFFIX}
   /sw/lib${LIB_SUFFIX}
   /usr/pkg/lib${LIB_SUFFIX}
   )
