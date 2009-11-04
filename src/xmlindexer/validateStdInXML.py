f9f859f09e189edb6484ede03cdc589bc2b2d339
Jos van den Oever <jos@vandenoever.info>
2009-11-04 16:50:01 +0000

    Show average speed at end of analysis.
    svn path=/trunk/kdesupport/strigi/; revision=1044796

--- src/xmlindexer/validateStdInXML.py
+++ src/xmlindexer/validateStdInXML.py
@@ -7,16 +7,23 @@ class UriLogger(handler.ContentHandler):
 
     def __init__(self):
         self.count = 0
-        self.start = time.time()
+        self.start = 0
 
     def startElement(self, name, attrs):
         if attrs.has_key('uri'):
+            if (self.start == 0):
+                self.start = time.time()
             self.uri = attrs['uri']
             self.count += 1
             if (self.count % 1000 == 0):
                 elapsed = time.time() - self.start
                 elapsed = self.count/elapsed
                 print '%9d %9d %s' % (self.count, elapsed, self.uri)
+
+    def endDocument(self):
+        elapsed = time.time() - self.start
+        elapsed = (self.count-1)/elapsed
+        print '%9d %9d' % (self.count, elapsed)
 
 # this script reads from standard input and parses it as xml
 # if the xml is invalid, it will print an error message
