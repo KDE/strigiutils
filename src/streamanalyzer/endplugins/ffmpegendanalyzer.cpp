6cd453d49758abb061beb4d9d3905b24cdc78e7f
Evgeny Egorochkin <phreedom.stdin@gmail.com>
2010-02-10 08:59:05 +0000

    FFMPEG-based media analyzer. Supports lots of metadata formats and is somewhat stream-friendly.
    svn path=/trunk/kdesupport/strigi/; revision=1088128

---
+++ src/streamanalyzer/endplugins/ffmpegendanalyzer.cpp
@@ -0,0 +1,423 @@
+/* This file is part of Strigi Desktop Search
+ *
+ * Copyright (C) 2010 Evgeny Egorochkin <phreedom.stdin@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public License
+ * along with this library; see the file COPYING.LIB.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#define STRIGI_IMPORT_API
+#include "analyzerplugin.h"
+#include "streamendanalyzer.h"
+#include "analysisresult.h"
+#include "fieldtypes.h"
+#include "textutils.h"
+#include "rdfnamespaces.h"
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+}
+#include <cstring>
+#include <iostream>
+#include <sstream>
+using namespace Strigi;
+using namespace std;
+
+class FFMPEGEndAnalyzerFactory;
+
+class STRIGI_PLUGIN_API FFMPEGEndAnalyzer : public StreamEndAnalyzer {
+private:
+    const FFMPEGEndAnalyzerFactory* factory;
+public:
+    FFMPEGEndAnalyzer(const FFMPEGEndAnalyzerFactory* f) :factory(f) {}
+    
+    ~FFMPEGEndAnalyzer() {}
+    
+    const char* name() const {
+        return "FFMPEGEndAnalyzer";
+    }
+    bool checkHeader(const char* header, int32_t headersize) const;
+    signed char analyze(AnalysisResult& idx, ::InputStream* in);
+};
+
+class STRIGI_PLUGIN_API FFMPEGEndAnalyzerFactory : public StreamEndAnalyzerFactory {
+friend class FFMPEGEndAnalyzer;
+private:
+    StreamEndAnalyzer* newInstance() const {
+        av_register_all();
+        return new FFMPEGEndAnalyzer(this);
+    }
+    const char* name() const {
+        return "FFMPEGEndAnalyzer";
+    }
+    void registerFields(FieldRegister& );
+
+    const RegisteredField* durationProperty;
+    const RegisteredField* widthProperty;
+    const RegisteredField* heightProperty;
+    const RegisteredField* frameRateProperty;
+    const RegisteredField* codecProperty;
+    const RegisteredField* bitrateProperty;
+    const RegisteredField* channelsProperty;
+    const RegisteredField* samplerateProperty;
+    const RegisteredField* titleProperty;
+    const RegisteredField* creatorProperty;
+    const RegisteredField* copyrightProperty;
+    const RegisteredField* commentProperty;
+    const RegisteredField* albumProperty;
+    const RegisteredField* genreProperty;
+    const RegisteredField* trackProperty;
+    const RegisteredField* createdProperty;
+    const RegisteredField* typeProperty;
+};
+
+const string
+  videoClassName = 
+    NFO "Video",
+  audioClassName = 
+    NFO "Audio",
+  musicPieceClassName = 
+    NMM_DRAFT "MusicPiece",
+  albumClassName =
+    NMM_DRAFT "MusicAlbum",
+
+  typePropertyName = 
+    RDF "type",
+
+  titlePropertyName = 
+    NIE "title",
+  commentPropertyName = 
+    NIE "comment",
+  languagePropertyName = 
+    NIE "language",
+  genrePropertyName =
+    NMM_DRAFT "genre",
+  trackPropertyName =
+    NMM_DRAFT "trackNumber",
+  createdPropertyName =
+    NIE "contentCreated",
+  creatorPropertyName =
+    NCO "creator",
+  copyrightPropertyName =
+    NIE "copyright",
+  albumPropertyName =
+    NMM_DRAFT "MusicAlbum",
+
+  sampleratePropertyName = 
+    NFO "sampleRate",
+  codecPropertyName = 
+    NFO "codec",
+  channelsPropertyName = 
+    NFO "channels",
+  bitratePropertyName = 
+    NFO "averageBitrate",
+  durationPropertyName = 
+    NFO "duration",
+  widthPropertyName = 
+    NFO "width",
+  heightPropertyName = 
+    NFO "height",
+  aspectRatioPropertyName =
+    NFO "aspectRatio",
+  frameRatePropertyName = 
+    NFO "frameRate";
+
+void
+FFMPEGEndAnalyzerFactory::registerFields(FieldRegister& r) {
+  durationProperty = r.registerField(durationPropertyName);
+  widthProperty = r.registerField(widthPropertyName);
+  heightProperty = r.registerField(heightPropertyName);
+  frameRateProperty = r.registerField(frameRatePropertyName);
+  codecProperty = r.registerField(codecPropertyName);
+  bitrateProperty = r.registerField(bitratePropertyName);
+  typeProperty = r.typeField;
+  channelsProperty = r.registerField(channelsPropertyName);
+  samplerateProperty = r.registerField(sampleratePropertyName);
+  titleProperty = r.registerField(titlePropertyName);
+  creatorProperty = r.registerField(creatorPropertyName);
+  copyrightProperty = r.registerField(copyrightPropertyName);
+  commentProperty = r.registerField(commentPropertyName);
+  albumProperty = r.registerField(albumPropertyName);
+  genreProperty = r.registerField(genrePropertyName);
+  trackProperty = r.registerField(trackPropertyName);
+  createdProperty = r.registerField(createdPropertyName);
+}
+
+// Input format is probed twice, but compared to expense of stream metadata extraction this isn't a huge deal.
+// Unfortunately you can't save probe results in checkHeader because it's const
+bool
+FFMPEGEndAnalyzer::checkHeader(const char* header, int32_t headersize) const {
+  AVProbeData pd;
+  pd.buf = (unsigned char*)header;
+  pd.buf_size = headersize;
+  pd.filename ="";
+
+  return av_probe_input_format(&pd, 1);
+}
+/*FIXME
+make it produce the same data in stream and file mode when possible.
+make sure it reports both duration and bitrate
+report stream sizes
+make sure there are no misdetections(something along the lines of av_prob_input_format2:
+    .log files in albums are often confused with media.
+*/
+
+extern "C" {
+int read_data(void *opaque, uint8_t *buf, int buf_size) {
+  cout<<"READ";
+  InputStream *s = (InputStream *) opaque;
+  if (!s)
+    return -1;
+
+  const char *sbuf;
+  cout<<s->position()<<" "<<flush;
+  int32_t len = s->read(sbuf, buf_size, buf_size);
+    cout<<s->position()<<" "<<buf_size<<" "<<len<<" "<<s->size()<<flush;
+  if (len>0)
+    memcpy( buf, sbuf, len);
+  cout<<" OK\n"<<flush;
+  return len;
+}
+
+int64_t seek_data(void *opaque, int64_t offset, int whence) {
+  InputStream *s = (InputStream *) opaque;
+  int64_t target = -1;
+  int64_t size;
+  
+  cout<<"SEEK"<<offset<<" "<<whence<<"\n"<<flush;
+  
+  if ( whence== SEEK_SET) {
+    target = offset;
+  } else if ( whence == SEEK_CUR ) {
+    target = s->position() + offset;
+  } else if ( (whence == SEEK_END) && (size = s->size()>=0) ) {
+    target = size+offset;
+  } else if ( whence == AVSEEK_SIZE ) {
+    return s->size();
+  } else
+    return -1;
+
+  int64_t t= s->reset(target);
+  cout<<t<<"\n"<<flush;
+  return (t == target ? target : -1);
+}
+}
+
+signed char
+FFMPEGEndAnalyzer::analyze(AnalysisResult& ar, ::InputStream* in) {
+  string filename;
+  if ((ar.depth()==0) && (ar.path().substr(0,7) == "file://")) 
+    filename = ar.path().substr(7);
+  else
+    filename = ar.path();
+
+  #define IO_BUFFER_SIZE 32768
+  uint8_t pDataBuffer[IO_BUFFER_SIZE];//65536];
+  long lSize = IO_BUFFER_SIZE;
+
+  ByteIOContext ByteIOCtx;
+  if(init_put_byte(&ByteIOCtx, pDataBuffer, lSize, 0, in, read_data, NULL, seek_data) < 0)
+    return -1;
+
+  //pAVInputFormat->flags |= AVFMT_NOFILE;
+  ByteIOCtx.is_streamed = 0;
+
+  AVProbeData pd;
+  const char *buf;
+  pd.filename ="";
+  pd.buf_size = in->read(buf, 1024,1024);
+  pd.buf = (unsigned char*)buf;
+  in->reset(0);
+
+  AVInputFormat* fmt = av_probe_input_format(&pd, 1);
+  
+  AVFormatContext *fc = NULL;
+  if(av_open_input_stream(&fc, &ByteIOCtx, "", fmt, NULL) < 0)
+    return -1;
+
+  av_find_stream_info(fc);
+  
+  // Dump information about file onto standard error
+  dump_format(fc, 0, filename.c_str(), false);
+
+  //FIXME if bitrate is not extracted, try estimating it as size/duration
+  if(fc->bit_rate)
+    ar.addValue(factory->bitrateProperty, fc->bit_rate);
+  
+  //FIXME if duration is not extracted, try estimating it as size/bitrate
+  if(fc->duration)
+    ar.addValue(factory->durationProperty,(uint32_t)(fc->duration / AV_TIME_BASE));
+  
+  if(fc->nb_streams==1 && fc->streams[0]->codec->codec_type == CODEC_TYPE_AUDIO) {
+    ar.addValue(factory->typeProperty, NFO "Audio");
+    ar.addValue(factory->typeProperty, NMM_DRAFT "MusicPiece");
+  } else {
+    ar.addValue(factory->typeProperty, NFO "Video");
+  }
+
+  for(uint32_t i=0; i<fc->nb_streams; i++) {
+    const AVStream &stream = *fc->streams[i];
+    const AVCodecContext &codec = *stream.codec;
+    
+    if (codec.codec_type == CODEC_TYPE_AUDIO || codec.codec_type == CODEC_TYPE_VIDEO) {
+      const string streamuri = ar.newAnonymousUri();
+      if (stream.duration && stream.time_base.num && stream.time_base.den) {
+        ostringstream outs;
+        outs << (stream.duration * stream.time_base.num / stream.time_base.den);
+        ar.addTriplet(streamuri, durationPropertyName,outs.str());
+      }
+      if (size_t len = strlen(stream.language)) {
+        ar.addTriplet(streamuri, languagePropertyName, string(stream.language, len));
+      }
+      const AVCodec *p = avcodec_find_decoder(codec.codec_id);
+      if (p) {
+        if (size_t len = strlen(p->name)) {
+          ar.addTriplet(streamuri, codecPropertyName, string(p->name, len));
+        }
+      } else if (size_t len = strlen(codec.codec_name)) {
+        ar.addTriplet(streamuri, codecPropertyName, string(codec.codec_name, len));
+      }
+/*
+00792     } else if (enc->codec_id == CODEC_ID_MPEG2TS) {
+// fake mpeg2 transport stream codec (currently not
+00794            registered)
+00795         codec_name = "mpeg2ts";
+00798     } else {
+00799         // output avi tags 
+00800         if(   isprint(enc->codec_tag&0xFF) && isprint((enc->codec_tag>>8)&0xFF)
+00801            && isprint((enc->codec_tag>>16)&0xFF) && isprint((enc->codec_tag>>24)&0xFF)){
+00802             snprintf(buf1, sizeof(buf1), "%c%c%c%c / 0x%04X",
+00803                      enc->codec_tag & 0xff,
+00804                      (enc->codec_tag >> 8) & 0xff,
+00805                      (enc->codec_tag >> 16) & 0xff,
+00806                      (enc->codec_tag >> 24) & 0xff,
+00807                       enc->codec_tag);
+00808         } else {
+00809             snprintf(buf1, sizeof(buf1), "0x%04x", enc->codec_tag);
+00810         }
+00811         codec_name = buf1;
+00812     }
+*/
+      if (codec.bit_rate) {
+        ostringstream outs;
+        outs << codec.bit_rate;
+        ar.addTriplet(streamuri, bitratePropertyName, outs.str());
+      }
+
+      if (codec.codec_type == CODEC_TYPE_AUDIO) {
+        
+        ar.addTriplet(streamuri, typePropertyName, audioClassName);
+        if (codec.channels) {
+          ostringstream outs;
+          outs << codec.channels;
+          ar.addTriplet(streamuri, channelsPropertyName, outs.str());
+        }
+        if (codec.sample_rate) {
+          ostringstream outs;
+          outs << codec.sample_rate;
+          ar.addTriplet(streamuri, sampleratePropertyName, outs.str());
+        }
+        if (codec.sample_fmt != SAMPLE_FMT_NONE) {}//FIXME sample format
+          
+      } else { // video stream
+        
+        ar.addTriplet(streamuri, typePropertyName, videoClassName);
+        if (codec.width) {
+          ostringstream outs;
+          outs << codec.width;
+          ar.addTriplet(streamuri, widthPropertyName, outs.str());
+          if (codec.sample_aspect_ratio.num) {
+            AVRational aspectratio;
+            ostringstream outs;
+            av_reduce(&aspectratio.num, &aspectratio.den,
+                      codec.width  * codec.sample_aspect_ratio.num,
+                      codec.height * codec.sample_aspect_ratio.den,
+                      1024*1024);
+            outs << aspectratio.num << ":" << aspectratio.den;
+            ar.addTriplet(streamuri, aspectRatioPropertyName, outs.str());
+          }
+        }
+        if (codec.height) {
+          ostringstream outs;
+          outs << codec.height;
+          ar.addTriplet(streamuri, heightPropertyName, outs.str());
+        }
+        if (stream.r_frame_rate.num && stream.r_frame_rate.den) {
+          ostringstream outs;
+          outs << stream.r_frame_rate.num / stream.r_frame_rate.den;
+          ar.addTriplet(streamuri, frameRatePropertyName, outs.str());
+        }
+        if (codec.pix_fmt != PIX_FMT_NONE) {}//FIXME pixel format
+      }
+      
+    }
+  }
+
+  // Tags
+  
+  if (int32_t len = strlen(fc->title)) {
+    ar.addValue(factory->titleProperty, string(fc->title, len) );
+  }
+  if (int32_t len = strlen(fc->author)) {
+    ar.addValue(factory->creatorProperty, string(fc->author, len) );
+  }
+  if (int32_t len = strlen(fc->copyright)) {
+    ar.addValue(factory->copyrightProperty, string(fc->copyright, len) );
+  }
+  if (int32_t len = strlen(fc->comment)) {
+    ar.addValue(factory->commentProperty, string(fc->comment, len) );
+  }
+  if (int32_t len = strlen(fc->album)) {
+    const string album = ar.newAnonymousUri();
+    ar.addValue(factory->albumProperty, album);
+    ar.addTriplet(album, typePropertyName, albumClassName);
+    ar.addTriplet(album, titlePropertyName, string(fc->album, len) );
+  }
+  if (int32_t len = strlen(fc->genre)) {
+    ar.addValue(factory->genreProperty, string(fc->genre, len) );
+  }
+  if (fc->track) {
+    ar.addValue(factory->trackProperty, fc->track);
+  }
+  if (fc->year) {
+    ar.addValue(factory->createdProperty, fc->year);
+  }
+
+  av_close_input_stream(fc);
+  //url_fclose(&ByteIOCtx);
+  
+  return 0;
+}
+
+/*
+ For plugins, we need to have a way to find out which plugins are defined in a
+ plugin. One instance of AnalyzerFactoryFactory per plugin profides this
+ information.
+*/
+class Factory : public AnalyzerFactoryFactory {
+public:
+    list<StreamEndAnalyzerFactory*>
+    streamEndAnalyzerFactories() const {
+        list<StreamEndAnalyzerFactory*> af;
+        af.push_back(new FFMPEGEndAnalyzerFactory());
+        return af;
+    }
+};
+
+/*
+ Register the AnalyzerFactoryFactory
+*/
+STRIGI_ANALYZER_FACTORY(Factory)
